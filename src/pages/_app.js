fd3fd3873c9ffe4113cabefe580f3437d10eb0ec
ThomasNasc <thomas.nascimento@intelitrader.com.br>
2023-05-01 21:53:59 -0300

    2.2: Corrigida estrutura de pastas

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,10 @@
 import "@/styles/globals.css";
 import "../styles/Home.module.css";
-import "../styles/components/AnimatedCicle.css";
+import "../styles/AnimatedCicle.css";
 import "../styles/index.css";
 import "../styles/about.css";
-import "../styles/components/Projects.css";
-import "../styles/components/contact.css";
+import "../styles/Projects.css";
+import "../styles/contact.css";
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
